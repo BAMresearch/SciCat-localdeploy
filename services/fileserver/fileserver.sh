395fde38384f0a71b4432b227bd07e89a257c647
Ingo Bressler <dev@ingobressler.net>
2019-04-25 14:45:29 +0200

    fileserver: use Dockerfile without proxy setting

--- services/fileserver/fileserver.sh
+++ services/fileserver/fileserver.sh
@@ -9,13 +9,22 @@ export LOCAL_ENV="${envarray[i]}"
 echo $LOCAL_ENV
 helm del --purge fileserver
 cd services/fileserver/
-if [ -d "./component/" ]; then
-  cd component
-  git pull
-else
+if [ ! -d "./component/" ]; then
   git clone $REPO component
-  cd component
 fi
+cd component
+git pull
+
+# create own site-specific Dockerfile (no proxy needed, as in $REPO)
+cat <<EOF > Dockerfile
+FROM ubuntu:bionic
+RUN apt-get update && apt-get install -y python3-tornado 
+COPY . /usr/src/app/
+WORKDIR /usr/src/app/
+EXPOSE 8888
+CMD ["python3","app2.py"]
+EOF
+
 export FILESERVER_IMAGE_VERSION=$(git rev-parse HEAD)
 docker build . -t $4:$FILESERVER_IMAGE_VERSION$LOCAL_ENV
 docker push $4:$FILESERVER_IMAGE_VERSION$LOCAL_ENV
@@ -23,6 +32,6 @@ echo "Deploying to Kubernetes"
 cd ..
 cd ..
 pwd
-echo helm install fileserver --name fileserver --namespace $LOCAL_ENV --set image.tag=$FILESERVER_IMAGE_VERSION$LOCAL_ENV --set image.repository=$4
-helm install fileserver --name fileserver --namespace $LOCAL_ENV --set image.tag=$FILESERVER_IMAGE_VERSION$LOCAL_ENV --set image.repository=$4
+cmd="helm install fileserver --name fileserver --namespace $LOCAL_ENV --set image.tag=$FILESERVER_IMAGE_VERSION$LOCAL_ENV --set image.repository=$4"
+echo "$cmd"; eval $cmd
 # envsubst < ../catanie-deployment.yaml | kubectl apply -f - --validate=false
