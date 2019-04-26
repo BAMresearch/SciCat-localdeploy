07c6c633d011b2a0ddbc3547420d9aa4497c4e1b
Ingo Bressler <dev@ingobressler.net>
2019-04-26 11:39:58 +0200

    start.sh: register minikube ip as docker.local early; create registry only if not present already

--- start.sh
+++ start.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+registerDockerIP()
+{
+    local hostsfn="/etc/hosts"
+    local dname="docker.local"
+    local ipaddr; ipaddr="$(minikube ip)"
+    sudo sed -i "/$dname/d" "$hostsfn"
+    sudo sh -c "echo '$ipaddr\t$dname' >> '$hostsfn'"
+    sudo service docker restart
+}
+
 if [ "$(uname)" == "Darwin" ]; then
-        LOCAL_IP=`ipconfig getifaddr en0`
+    LOCAL_IP=`ipconfig getifaddr en0`
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        LOCAL_IP=`hostname --ip-address`
+    LOCAL_IP=`hostname --ip-address`
 fi
 echo $LOCAL_IP
-#minikube start -v7  --insecure-registry localhost:5000 --extra-config=apiserver.GenericServerRunOptions.AuthorizationMode=RBAC
+#minikube start -v7    --insecure-registry localhost:5000 --extra-config=apiserver.GenericServerRunOptions.AuthorizationMode=RBAC
 
 minikube start --insecure-registry docker.local:5000
 #kubectl config use-context minikube #should auto set, but added in case
+registerDockerIP # docker.local points always to the same local registry
 
 #kubectl -n kube-system create sa tiller # handled by rbac-config.yaml
 kubectl create -f rbac-config.yaml
@@ -21,24 +32,21 @@ sleep 5
 kubectl apply -f service-nodeport.yaml
 kubectl apply -f configmap.yaml
 
-
 NS_DIR=./namespaces/*.yaml
 
 for file in $NS_DIR; do
-  f="$(basename $file)"
-  ns="${f%.*}"
-  kubectl delete namespace $ns 2> /dev/null
+    f="$(basename $file)"
+    ns="${f%.*}"
+    kubectl delete namespace $ns 2> /dev/null
 done
 
 # set up a local registry
-# https://hackernoon.com/local-kubernetes-setup-with-minikube-on-mac-os-x-eeeb1cbdc0b
-# let docker context point to minikube
-eval $(minikube docker-env)
-# start local docker registry
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
-# https://stackoverflow.com/a/54190375
-DNAME="docker.local"
-IPADDR="$(minikube ip)"
-sudo sed -i "/$DNAME/d" /etc/hosts
-sudo sh -c "echo '$IPADDR\t$DNAME' >> /etc/hosts"
-sudo service docker restart
+if [ ! -z "$(docker image ls -q registry)" ]; then
+    # https://hackernoon.com/local-kubernetes-setup-with-minikube-on-mac-os-x-eeeb1cbdc0b
+    # let docker context point to minikube
+    eval $(minikube docker-env)
+    # start local docker registry
+    docker run -d -p 5000:5000 --restart=always --name registry registry:2
+fi
+
+# vim: set ts=4 sw=4 sts=4 tw=0 et:
