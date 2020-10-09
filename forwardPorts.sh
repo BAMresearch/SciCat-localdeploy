cd0c470b21f277792b0cdf548bf7c0ed1cdae514
Ingo Breßler <dev@ingobressler.net>
2020-10-09 19:49:38 +0200

    fixed ssh port forwarding script

--- forwardPorts.sh
+++ forwardPorts.sh
@@ -2,12 +2,12 @@
 
 ipaddr="$(minikube ip)"
 sudo killall ssh 2>/dev/null
-sudo sh -c "
-    mkdir -p ~\$(whoami)/.ssh;
-    fn=\$(eval echo ~\$(whoami)/.ssh/known_hosts);
+sudo sh -xc "
+    mkdir -p ~/.ssh;
+    fn=\$(eval echo ~/.ssh/known_hosts);
     ssh-keygen -f \$fn -R '$ipaddr';
     ssh-keyscan -H '$ipaddr' >> \$fn;
-    ssh -N -i ~/.minikube/machines/minikube/id_rsa -L 0.0.0.0:80:$ipaddr:80 docker@$ipaddr & \
-    ssh -N -i ~/.minikube/machines/minikube/id_rsa -L 0.0.0.0:443:$ipaddr:443 docker@$ipaddr &"
+    ssh -N -i $HOME/.minikube/machines/minikube/id_rsa -L 0.0.0.0:80:$ipaddr:80 docker@$ipaddr & \
+    ssh -N -i $HOME/.minikube/machines/minikube/id_rsa -L 0.0.0.0:443:$ipaddr:443 docker@$ipaddr &"
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
