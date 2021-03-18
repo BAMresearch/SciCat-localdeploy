7ccafcdb0aa5fa9dab04dc28d3845ce0d99595b3
Ingo Breßler <dev@ingobressler.net>
2021-03-18 16:11:48 +0000

    registry: moved storage definitions to separate folder

--- 02_registry.sh
+++ 02_registry.sh
@@ -2,11 +2,13 @@
 
 # learn about some utility functions before heading on ...
 scriptpath="$(readlink -f "$0")"
-. "$(dirname "$scriptpath")"/services/deploytools
+scriptdir="$(dirname "$scriptpath")"
+. "$scriptdir/services/deploytools"
 
 # CERT_PATH_PUB should be the path to the full chain public cert
 checkVars REGISTRY_PORT CERT_PATH_PUB CERT_PATH_PRIV || exit 1
 REGISTRY_NAME=myregistry
+pvcfg="$scriptdir/definitions/registry_pv_nfs.yaml"
 
 if [ "$1" != "clean" ];
 then
@@ -15,7 +17,7 @@ then
     # add registry name to known hosts -> on all nodes which access the registry
 #    grep -q $REGISTRY_NAME /etc/hosts || sudo sed -i -e '/10.0.9.1/s/$/ '$REGISTRY_NAME'/' /etc/hosts
     sudo -E kubectl create secret -ndev tls "${REGISTRY_NAME}.secret" --key "$CERT_PATH_PRIV" --cert "$CERT_PATH_PUB"
-    kubectl apply -f registry_pv_nfs.yaml
+    kubectl apply -f "$pvcfg"
     helm install $REGISTRY_NAME twuni/docker-registry --namespace dev \
         --set service.type=NodePort,service.nodePort=$REGISTRY_PORT \
         --set tlsSecretName="${REGISTRY_NAME}.secret" \
@@ -23,7 +25,7 @@ then
 else # clean up
     helm del $REGISTRY_NAME -ndev
     kubectl delete secret -ndev "${REGISTRY_NAME}.secret"
-    kubectl delete pv pvregistry
+    kubectl delete -f "$pvcfg"
 fi
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
