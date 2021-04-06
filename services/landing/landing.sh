8306159e5e79c71a35e293b2cc5050bd687ab4c0
Ingo Breßler <ingo.bressler@bam.de>
2021-04-06 15:44:40 +0000

    landing: setup script lined up with catanie setup

--- services/landing/landing.sh
+++ services/landing/landing.sh
@@ -1,84 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-source ./services/deploytools
+# get the script directory before creating any files
+scriptdir="$(dirname "$(readlink -f "$0")")"
+. "$scriptdir/../deploytools"
 
-[ -z "$DOCKER_REG" ] && \
-    echo "WARNING: Docker registry not defined, using default (docker.io?)!"
-docker_repo="$DOCKER_REG/ls"
+# get given command line flags
+noBuild="$(getScriptFlags nobuild "$@")"
+buildOnly="$(getScriptFlags buildonly "$@")"
+clean="$(getScriptFlags clean "$@")"
+
+loadSiteConfig
+checkVars SC_LANDING_FQDN SC_LANDING_FQDN SC_LANDING_PUB SC_LANDING_KEY SC_REGISTRY_ADDR || exit 1
 
 REPO="https://github.com/SciCatProject/LandingPageServer.git"
-export LOCAL_ENV=$SC_NAMESPACE # selects angular configuration in subrepo component
-cd ./services/landing/
-
-INGRESS_NAME=" "
-DOCKERNAME="-f ./Dockerfile"
-if [ "$(hostname)" == "kubetest01.dm.esss.dk" ]; then
-    LOCAL_ENV=dmsc
-    INGRESS_NAME="-f ./landingserver/dmsc.yaml"
-    DOCKERNAME="-f ./CI/ESS/Dockerfile.dmsc"
-elif    [ "$(hostname)" == "scicat01.esss.lu.se" ]; then
-    LOCAL_ENV=ess
-    INGRESS_NAME="-f ./landingserver/lund.yaml"
-    DOCKERNAME="-f ./CI/ESS/Dockerfile.ess"
-elif    [ "$(hostname)" == "k8-lrg-serv-prod.esss.dk" ]; then
-    LOCAL_ENV=dmscprod
-    INGRESS_NAME="-f ./landingserver/dmscprod.yaml"
-    DOCKERNAME="-f ./CI/ESS/Dockerfile.dmscprod"
-else
-    YAMLFN="./landingserver/$(hostname).yaml"
-    INGRESS_NAME="-f $YAMLFN"
-    # generate yaml file with appropriate hostname here
-    cat > "$YAMLFN" << EOF
-ingress:
-    enabled: true
-    host: landing.$(hostname --fqdn)
-EOF
-fi
-
-hostaddr="$(getHostAddr)"
-
-read -r -d '' angCfg <<EOF
-{
-    "optimization": true,
-    "outputHashing": "all",
-    "sourceMap": false,
-    "extractCss": true,
-    "namedChunks": false,
-    "aot": true,
-    "extractLicenses": true,
-    "vendorChunk": false,
-    "buildOptimizer": true,
-    "fileReplacements": [ {
-        "replace": "src/environments/environment.ts",
-        "with": \$envfn } ]
-}
-EOF
-
-helm del landingserver -n$LOCAL_ENV
-if [ ! -d "./component/" ]; then
-    git clone $REPO component
-fi
-cd component
-git checkout . # revert any changes so that pull succeeds
-git pull
-git checkout -f develop
-git clean -f
-# update angular config
-lbBaseURL="http://catamel.$(hostname --fqdn)"
-angEnv=$(sed -e "/facility/ s/'[^']\+',/'BAM',/" \
-             -e "/lbBaseURL/ s#'[^']\+',#'$lbBaseURL',#" \
-             src/environments/environment.dmscprod.ts)
-injectEnvConfig LandingPageServer "$LOCAL_ENV" "$angEnv" "$angCfg"
-
-# using ESS Dockerfile and modify it to our needs
-grep -v '^$' CI/ESS/Dockerfile.dmscprod \
-    | grep -v proxy \
-    | grep -v 'npm config set' \
-    | grep -v 'COPY CI/ESS/' \
-    | sed -e '/LB_BASE_URL=/ s#\(http://\)[^\]\+\(/.*\)#\1'$hostaddr:3000'\2#' \
-          -e "/RUN ng build/ s/dmscprod/$LOCAL_ENV/" \
-          -e "/ARG env/ s/env=\\s*[a-zA-Z0-9]\+/env=$LOCAL_ENV/" \
-    > Dockerfile
 
 copyimages()
 {
@@ -86,48 +20,88 @@ copyimages()
         echo "$0 not in directory 'component', aborting!"
         return
     fi
-    local mediaPath="../../../media"
-    if [ ! -d "$mediaPath" ]; then
+    if [ ! -d "$SC_MEDIA_PATH" ]; then
         echo "No media/images found, not copying site specific media."
         return
     fi
     # get favicon
-    local favicon="$mediaPath/favicon.ico"
+    local favicon="$SC_MEDIA_PATH/favicon.ico"
     if [ ! -f "$favicon" ]; then
-        (cd "$mediaPath" && convert src/icon.svg -define icon:auto-resize="64,48,32,16" favicon.ico)
+        (cd "$SC_MEDIA_PATH" && convert src/icon.svg -define icon:auto-resize="64,48,32,16" favicon.ico)
     fi
     [ -f "$favicon" ] && cp "$favicon" src/favicon.ico
-    if [ -f "$mediaPath/src/icon.svg" ]; then
-        convert "$mediaPath/src/icon.svg" -resize 16 src/favicon-16x16.png
-        convert "$mediaPath/src/icon.svg" -resize 32 src/favicon-32x32.png
+    if [ -f "$SC_MEDIA_PATH/src/icon.svg" ]; then
+        convert "$SC_MEDIA_PATH/src/icon.svg" -resize 16 src/favicon-16x16.png
+        convert "$SC_MEDIA_PATH/src/icon.svg" -resize 32 src/favicon-32x32.png
     fi
-    if [ -f "$mediaPath/src/logo.svg" ]; then
-        convert "$mediaPath/src/logo.svg" -resize 192 src/android-chrome-192x192.png
-        convert "$mediaPath/src/logo.svg" -resize 384 src/android-chrome-384x384.png
-        convert "$mediaPath/src/logo.svg" -resize 180 src/apple-touch-icon.png
-        convert "$mediaPath/src/logo.svg" -resize 150 src/mstile-150x150.png
-        cp "$mediaPath/src/logo.svg" src/safari-pinned-tab.svg
+    if [ -f "$SC_MEDIA_PATH/src/logo.svg" ]; then
+        convert "$SC_MEDIA_PATH/src/logo.svg" -resize 192 src/android-chrome-192x192.png
+        convert "$SC_MEDIA_PATH/src/logo.svg" -resize 384 src/android-chrome-384x384.png
+        convert "$SC_MEDIA_PATH/src/logo.svg" -resize 180 src/apple-touch-icon.png
+        convert "$SC_MEDIA_PATH/src/logo.svg" -resize 150 src/mstile-150x150.png
+        cp "$SC_MEDIA_PATH/src/logo.svg" src/safari-pinned-tab.svg
     fi
-    mediaPath="$mediaPath/landing"
+    mediaPath="$SC_MEDIA_PATH/landing"
     local bannersrc; bannersrc="$(find $mediaPath -maxdepth 1 -iname '*banner*.png' | head -n1)"
     [ -f "$bannersrc" ] && cp "$bannersrc" src/assets/site_banner.png
 }
 
-export LANDING_IMAGE_VERSION=$(git rev-parse HEAD)
-echo $DOCKERNAME
-copyimages
-if [ "$(hostname)" != "k8-lrg-serv-prod.esss.dk" ]; then
-    cmd="docker build $DOCKERNAME . -t $docker_repo:$LANDING_IMAGE_VERSION$LOCAL_ENV"
-    echo "$cmd"; eval $cmd
-    cmd="docker push $docker_repo:$LANDING_IMAGE_VERSION$LOCAL_ENV"
-    echo "$cmd"; eval $cmd
+cd "$scriptdir"
+
+if [ -z "$buildOnly" ]; then
+    namespaceExists "$NS" || kubectl create ns "$NS"
+    # remove the existing service
+    helm del landingserver -n "$NS"
+    kubectl -n $NS delete secret certs-landing
+    [ -z "$clean" ] || exit 0 # stop here when cleaning up
+
+    IARGS="--set ingress.enabled=true,ingress.host=$SC_LANDING_FQDN,ingress.tlsSecretName=certs-catanie"
+    createTLSsecret "$NS" certs-landing "$SC_LANDING_PUB" "$SC_LANDING_KEY"
 fi
-echo "Deploying to Kubernetes"
-cd ..
-pwd
-cmd="helm install landingserver landingserver --namespace $LOCAL_ENV \
-    --set image.tag=$LANDING_IMAGE_VERSION$LOCAL_ENV --set image.repository=$docker_repo ${INGRESS_NAME}"
-echo "$cmd"; eval $cmd
-# envsubst < ../catanie-deployment.yaml | kubectl apply -f - --validate=false
+
+baseurl="$SC_REGISTRY_ADDR"
+IMG_NAME="landing-$NS"
+IMG_REPO="$baseurl/$IMG_NAME"
+# extra arguments if the registry need authentication as indicated by a set password
+[ -z "$SC_REGISTRY_PASS" ] || baseurl="$SC_REGISTRY_USER:$SC_REGISTRY_PASS@$baseurl"
+# get the latest image tag: sort by timestamp, pick the largest
+IMAGE_TAG="$(curl -s "https://$baseurl/v2/$IMG_NAME/tags/list" | jq -r '(.tags|sort[-1])?')"
+if [ -z "$noBuild" ] || [ -z "$IMAGE_TAG" ]; then
+    if [ ! -d "./component/" ]; then
+        git clone $REPO component
+    fi
+    cd component
+    git checkout develop
+    git checkout .
+    git clean -f
+    git pull
+    # update angular config
+    angEnv=$(sed -e "/facility:/s/[[:alnum:]\"]\+,$/\"$SC_SITE_NAME\",/g" \
+                 -e '/lbBaseURL:/s#[[:alnum:]"\:\./]\+,$#"https://'$SC_CATAMEL_FQDN'",#g' \
+                 -e '/scicatBaseUrl:/s#[[:alnum:]"\:\./]\+,$#"https://'$SC_CATANIE_FQDN'",#g' \
+                 src/environments/environment.essprod.ts)
+    angBuildCfg="$(jq '.projects.LandingPageServer.architect.build.configurations.essprod' angular.json)"
+    injectEnvConfig LandingPageServer "$NS" "$angEnv" "$angBuildCfg"
+    copyimages
+    echo "Building release"
+    sed '/_proxy/d;/maintainer/d;/site.png/d;/google/d;s/^\(ARG\s\+env=\).*$/\1'$NS'/' \
+        CI/ESS/Dockerfile.dmscprod > Dockerfile
+    IMAGE_TAG="$(git show --format='%at_%h' HEAD)" # <timestamp>_<git commit>
+    cmd="$DOCKER_BUILD -t $IMG_REPO:$IMAGE_TAG -t $IMG_REPO:latest --build-arg env=$NS ."
+    echo "$cmd"; eval $cmd
+    authargs="$(registryLogin)"
+    cmd="$DOCKER_PUSH $authargs $IMG_REPO:$IMAGE_TAG"
+    echo "$cmd"; eval $cmd
+    cd ..
+fi
+if [ -z "$buildOnly" ]; then
+    setRegistryAccessForPulling
+    echo "Deploying to Kubernetes"
+    cmd="helm install landingserver landingserver --namespace $NS --set image.tag=$IMAGE_TAG \\
+             --set image.repository=$IMG_REPO --set service.type=ClusterIP \\
+             ${IARGS}"
+    (echo "$cmd" && eval "$cmd")
+fi
+registryLogout
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
