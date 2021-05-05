440e2a00a5068e7a91c0644424576fd4673f7bc3
Ingo Breßler <dev@ingobressler.net>
2021-05-05 19:35:13 +0200

    mongodb script updated

--- 00_mongo.sh
+++ 00_mongo.sh
@@ -15,10 +15,15 @@ NS="$(sed -n -e '/^metadata/{:a;n;s/^\s\+name:\s*\(\w\+\)/\1/;p;Ta' -e'}' "$NS_F
 [ -z "$NS" ] && (echo "Could not determine namespace!"; exit 1)
 
 pvcfg="$scriptdir/definitions/mongo_pv_hostpath.yaml"
-if [ "$2" = "bare" ]; then
+if [ "$1" = "bare" ]; then
     pvcfg="$scriptdir/definitions/mongo_pv_nfs.yaml"
     echo " -> Using NFS for persistent volumes in 'bare' mode."
     echo "    Please make sure the configured NFS shares can be mounted: '$pvcfg'"
+    mpath="$(awk -F':' '/path:/{sub(/^ */,"",$2);print $2}' "$pvcfg")"
+    if ! [ -d "$mpath" ]; then
+        mkdir -p "$mpath"
+        chmod a+w "$mpath"
+    fi
 fi
 
 # remove the pod
@@ -28,7 +33,7 @@ pvname="$(kubectl -n $NS get pv | grep mongo | awk '{print $1}')"
 [ -z "$pvname" ] || \
     kubectl patch pv "$pvname" -p '{"spec":{"claimRef":null}}'
 
-if [ "$2" = "clean" ]; then
+if ([ "$1" = "clean" ] || [ "$2" = "clean" ]); then
     # delete old volume first
     echo -n "Waiting for mongodb persistentvolume being removed ... "
     while kubectl -n "$NS" get pv | grep -q mongo; do
@@ -46,7 +51,15 @@ if [ "$1" = "bare" ] && [ "$2" = "clean" ]; then
     [ -d "$datapath" ] && rm -R "$datapath/data"
 fi
 
+([ "$1" = "clean" ] || [ "$2" = "clean" ]) && exit
+
 kubectl apply -f "$pvcfg"
+# reset root password in existing db:
+# - create pod with auth disabled, helm arg '--set auth.enabled=false'
+# - change pwd of user root in db
+# - recreate pod with auth enabled
+# - update k8s secret (example pwd 'test'):
+#   kubectl -ndev get secret local-mongodb -o json | jq ".data[\"mongodb-root-password\"]=\"$(echo test | base64)\"" | kubectl apply -f -
 cmd="helm install local-mongodb bitnami/mongodb --namespace $NS"
 echo "$cmd"; eval $cmd
 
