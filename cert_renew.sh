3ba6297a417a19cb50608fd6fe7765ff87e31da0
Ingo Breßler <ingo.bressler@bam.de>
2022-03-29 16:42:13 +0200

    cert_renew simplified, wildcard domains seem to work now

--- cert_renew.sh
+++ cert_renew.sh
@@ -11,13 +11,33 @@ if [ ! -d "$le_wd" ]; then
   echo "Let's encrypt working dir not found!"
   exit 1
 fi
-domains="$(echo $DOMAINBASE; env | awk -F'=' "/\\.$DOMAINBASE/{print \$2}" | sort | uniq)"
+
+waitdelay=300 # for trying again, in secs
+
+# getting cert for wildcard subsubdomains
+getCert()
+{
+  cmd="$le_wd/acme.sh --home $le_wd --issue --dns dns_ddnss -d $1"
+  maxtries=5 # secs before failing hard
+  (echo "$cmd"; eval "$cmd")
+  return # skip the rest for now ...
+  while ! (echo "$cmd"; eval "$cmd"); do
+    echo "Waiting $waitdelay secs ..."; sleep $waitdelay
+    [ "$maxtries" -eq 0 ] && break #exit 1
+    maxtries=$((maxtries-1))
+  done
+}
+
+getCert "*.$DOMAINBASE"
+getCert "$DOMAINBASE"
+exit
+
+domains="$(env | grep -oE "[a-zA-Z0-9_\\-]+\\.$DOMAINBASE" | sort | uniq)"
 echo "Running certificate renewal for the following domain names:"
 echo "$domains"
 domargs=""
-waitdelay=300 # in secs
 [ -z "$nodelay" ] || waitdelay=0
-maxtries=5 # secs before failing hard
+exit
 for dom in $domains; do
   domargs="$domargs -d $dom"
   #echo "$domargs"
