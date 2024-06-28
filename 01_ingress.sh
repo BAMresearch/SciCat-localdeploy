abe13b7ff6a1c33f6374e184938b9fef9be3bc41
Ingo Breßler <ingo.bressler@bam.de>
2024-06-28 11:25:19 +0200

    ingress config: minor update

--- 01_ingress.sh
+++ 01_ingress.sh
@@ -19,7 +19,10 @@ if [ -z "$clean" ]; then
     ipaddr="$(ip addr show | awk '/\<inet\>\s[0-9\.]+\/24/ { split($2,a,"/"); print a[1] }' | head -n1)"
     helm install ingress-nginx ingress-nginx/ingress-nginx --namespace kube-system \
         --set controller.kind=DaemonSet --set "controller.service.externalIPs[0]=$ipaddr" \
-        --set "controller.config.worker-processes=$proccount"
+        --set controller.config.hsts-preload=true \
+        --set "controller.config.worker-processes=$proccount" \
+
+        #--set controller.extraArgs.v=5
 else # clean up
     helm del --namespace kube-system ingress-nginx
 fi
