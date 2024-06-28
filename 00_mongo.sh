ea8fabc1d50056600a4affca18693d353be38bc7
Ingo Breßler <ingo.bressler@bam.de>
2024-06-28 16:48:24 +0200

    mongodb script to set root password in the db

--- 00_mongo.sh
+++ 00_mongo.sh
@@ -43,21 +43,33 @@ if ! [ -d "$mpath" ]; then
     chmod a+w "$mpath"
 fi
 
+get_podid() {
+    kubectl get po -n dev | grep -o '[a-zA-Z0-9-]*mongo[a-zA-Z0-9-]*'
+}
 # remove the pod
-pvname="$(kubectl get pvc -n $NS local-mongodb -o jsonpath='{.spec.volumeName}')"
-helm del local-mongodb --namespace "$NS"
-# reclaim PV
-if [ ! -z "$pvname" ]; then
-    kubectl patch pv "$pvname" -p '{"spec":{"claimRef":null}}'
-    # delete old volume first
-    echo "Waiting for mongodb persistentvolume being removed ... "
-    while kubectl -n "$NS" get pv | grep -q mongo; do
-        # https://github.com/kubernetes/kubernetes/issues/77258#issuecomment-502209800
-        kubectl patch pv "$pvname" -p '{"metadata":{"finalizers":null}}'
-        timeout 6 kubectl delete pv "$pvname"
-    done
-    echo "done."
-fi
+svc=local-mongodb
+remove_pod() {
+    local svc="$1"
+    podid="$(get_podid)"
+    pvname="$(kubectl get pv -n $NS -o json | \
+        jq -r "(.items[] | select(.spec.claimRef.name==\"$svc\")).metadata.name")"
+    helm del $svc --namespace "$NS"
+    # reclaim PV
+    if [ ! -z "$pvname" ]; then
+        kubectl patch pv "$pvname" -p '{"spec":{"claimRef":null}}'
+        # delete old volume first
+        echo "Waiting for mongodb persistentvolume being removed ... "
+        while kubectl -n "$NS" get pv | grep -q mongo; do
+            # https://github.com/kubernetes/kubernetes/issues/77258#issuecomment-502209800
+            kubectl patch pv "$pvname" -p '{"metadata":{"finalizers":null}}'
+            timeout 6 kubectl delete pv "$pvname"
+        done
+        kubectl -n dev wait --for=delete "pvc/$svc"
+        echo "done."
+    fi
+    kubectl -n dev wait --for=delete "pod/$podid"
+}
+remove_pod $svc
 
 if [ ! -z "$deletedata" ]; then
     echo "Delete the underlying data!"
@@ -67,6 +79,7 @@ fi
 
 [ -z "$cleanonly" ] || exit # done here in 'clean only' mode
 
+# create the persistent volume first
 adjustServerAddr "$NFS_SERVER" "$pvcfg" | kubectl apply -f -
 # reset root password in existing db:
 # - restart service with auth disabled
@@ -78,9 +91,27 @@ adjustServerAddr "$NFS_SERVER" "$pvcfg" | kubectl apply -f -
 # - update k8s secret, set MONGODB_ROOT_PASSWORD env var before:
 #   kubectl -ndev get secret local-mongodb -o json | jq ".data[\"mongodb-root-password\"]=\"$(echo "$MONGODB_ROOT_PASSWORD" | base64)\"" | kubectl apply -f -
 
-autharg=""
-[ -z "$noauth" ] || autharg="--set auth.enabled=false"
-cmd="helm install local-mongodb bitnami/mongodb --namespace $NS $autharg"
+# start mongodb in no-auth mode first
+cmd="helm install $svc bitnami/mongodb --namespace $NS"
+tmpcmd="$cmd --set auth.enabled=false"
+echo "$tmpcmd"; eval $tmpcmd
+podid="$(get_podid)"
+kubectl -n dev wait --for=condition=ready "pod/$podid"
+# set root password in no-auth mode
+(echo "use admin"; echo "db.changeUserPassword(\"root\", \"$SC_MONGO_ROOTPWD\")") | \
+    kubectl -n dev exec -i "$podid"  -- mongosh
+remove_pod $svc
+kubectl get po -n dev
+kubectl get pv -n dev
+kubectl get pvc -n dev
+
+# create a new persistent volume first
+adjustServerAddr "$NFS_SERVER" "$pvcfg" | kubectl apply -f -
+# restart mongodb with auth again
 echo "$cmd"; eval $cmd
+kubectl -n dev wait --for=condition=ready "pod/$(get_podid)"
+
+# update k8s secret, set MONGODB_ROOT_PASSWORD env var before:
+kubectl -n $NS get secret $svc -o json | jq ".data[\"mongodb-root-password\"]=\"$(echo "$SC_MONGO_ROOTPWD" | base64)\"" | kubectl apply -f -
 
 # vim: set ts=4 sw=4 sts=4 tw=0 et:
